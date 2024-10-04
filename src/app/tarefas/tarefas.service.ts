00a219405f79bf8dd4fbd83f8af92ce787ed5a7f
Guilherme <guifalcao2017@gmail.com>
2024-10-04 13:48:53 -0300

    Criação da funçao no serviço de editar tarefas

--- src/app/tarefas/tarefas.service.ts
+++ src/app/tarefas/tarefas.service.ts
@@ -23,4 +23,8 @@ export class TarefasService {
   deleteTarefas(id: number): Observable<any> {
     return this.http.delete(`${this.apiUrl}/${id}`);
   }
+
+  editarTarefas(id: number, tarefa: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, tarefa);
+  }
 }
