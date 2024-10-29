f1b67b63a8f27f37676ef5b88ab11d49ab86272b
Guilherme <guifalcao2017@gmail.com>
2024-10-29 13:37:13 -0300

    Formatação

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,43 +5,36 @@ import { LoginService } from './login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
   loginForm: FormGroup;
   token;
 
-  constructor(
-    private fb: FormBuilder,
-    private loginService: LoginService
-  )
-  {
+  constructor(private fb: FormBuilder, private loginService: LoginService) {
     this.loginForm = this.fb.group({
       nome: [''],
-      senha: ['']
-    })
+      senha: [''],
+    });
   }
 
-  fazerLogin(){
+  fazerLogin() {}
+
+  cadastrar(nome: string, senha: string) {
+    if (!nome || !senha) {
+      alert('Preencha todos os campos!');
+    } else {
+      this.loginService.cadastrar(nome, senha).subscribe(
+        (data) => {
+          alert(
+            'Usúario cadastrado com sucesso! Você já pode fazer login para acessar sua lista de tarefas'
+          );
+        },
+        () => {
+          alert('Erro ao cadastrar usuário');
+        }
+      );
+      this.loginForm.reset();
+    }
   }
-
-
-
-  cadastrar(nome: string, senha: string){
-
-    if(!nome || !senha){
-      alert("Preencha todos os campos!");
-    } else{
-        this.loginService.cadastrar(nome, senha).subscribe(
-          (data)=>{
-            alert('Usúario cadastrado com sucesso!')
-          },
-          ()=>{
-            alert('Erro ao cadastrar usuário')
-          })
-      this.loginForm.reset()
-   }
-  }
-
 }
